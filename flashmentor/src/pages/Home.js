324f1fd5467618b57e2597f2399a6833befd5f84
an-siuu-man <ansuman_sharma@protonmail.com>
2024-10-26 22:36:41 -0500

    added onclick flip feature to homepage card

--- flashmentor/src/pages/Home.js
+++ flashmentor/src/pages/Home.js
@@ -1,14 +1,10 @@
 import FlashCard from "../components/FlashCard";
 import PageButton from "../components/PageButton";
-
+import { useState } from "react";
 export default function Home() {
 
+    const [homeCardFlipped, setHomeCardFlipped] = useState(false);
 
-
-    document.addEventListener('DOMContentLoaded', function() {
-        var homeCard = document.querySelector('.home-card');
-        
-    });
 
     return (
             <div className='flex justify-between '>
@@ -23,7 +19,8 @@ export default function Home() {
                     </div>
                 </div>
                 <div className="right-container p-[5%] w-[50%]"  >  
-                    <div className="home-card flex justify-center items-center w-[75%] h-[350px] p-[30px] bg-red-400 rounded-[40px] shadow-2xl">
+                    <div className={`home-card${homeCardFlipped ? '-flipped' : ''} flex justify-center items-center w-[75%] h-[350px] p-[30px] bg-red-400 rounded-[40px] shadow-2xl cursor-pointer`}
+                    onClick={() => {setHomeCardFlipped(!homeCardFlipped)}}>
                         <p className="font-[Poppins] text-2xl text-white text-center font-[600]">What is the best part of online learning?</p>
                     </div>
                 </div>
