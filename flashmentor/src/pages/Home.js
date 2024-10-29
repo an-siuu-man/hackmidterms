bd30ae1ab8535febfe3e1ee79e53b9ee43273771
an-siuu-man <ansuman_sharma@protonmail.com>
2024-10-29 14:32:53 -0500

    added some minor ui and animation changes to home page

--- flashmentor/src/pages/Home.js
+++ flashmentor/src/pages/Home.js
@@ -4,14 +4,18 @@ import { useState } from "react";
 export default function Home() {
     
     useEffect(() => {
-        // Adds the fade-in class to elements when the component is mounted
         const homeCard = document.querySelector('.home-card');
         const homeCardTwo = document.querySelector('.home-card-two');
-
+        const leftContainer = document.querySelector('.left-container');
         if (homeCard && homeCardTwo) {
             homeCard.classList.add('fade-in');
-            homeCardTwo.classList.add('fade-in');
+            homeCardTwo.classList.add('fade-in-two');
         }
+
+        if (leftContainer) {
+            leftContainer.classList.add('fade-in-three');
+        }
+
     }, []);
 
     return (
@@ -27,11 +31,11 @@ export default function Home() {
                 </div>
             </div>
             <div className="right-container p-[5%] w-[50%]">  
-                <div className="home-card flex justify-center items-center w-[75%] h-[350px] p-[30px] rounded-[40px] shadow-2xl">
-                    <p className="font-[Poppins] text-2xl text-white text-center font-[600]">What is the best part of online learning?</p>
+                <div className="home-card-two flex justify-center items-center w-[70%] h-[300px] p-[30px] rounded-[40px] shadow-2xl cursor-default">
+                    <p className="font-[Poppins] text-2xl text-white text-center font-[600]">Ready to optimize your online learning?</p>
                 </div>
-                <div className="home-card-two flex justify-center items-center w-[70%] h-[300px] rounded-[40px] shadow-2xl">
-                    <p className="font-[Poppins] text-2xl text-white text-center font-[600]">How can you optimize your online learning?</p>
+                <div className="home-card flex justify-center items-center w-[75%] h-[350px] p-[30px] rounded-[40px] shadow-2xl cursor-default">
+                    <p className="font-[Poppins] text-2xl text-white text-center font-[600]">What is the best part of online learning?</p>
                 </div>
             </div>
         </div>
